fba2aa8d61a0bac2c970862c5de94515c0c6dd9e
Ulises Venegas <uvenegas123@gmail.com>
2024-11-03 14:43:21 -0600

    first commit

--- Leap Year Explorer.py
+++ Leap Year Explorer.py
@@ -1,20 +1,16 @@
-# year = int(input('Enter a year:\n'))
-# leap_year = True
-# if year % 4 == 0:
-#     leap_year = True
-# elif year % 400 == 0:
-#     leap_year = True
-# elif year % 100 == 0:
-#     leap_year = False
-# else:
-#     leap_year = False
+year = int(input('Enter a year:\n'))
+leap_year = True
+if year % 400 == 0:
+    leap_year = True
+elif year % 100 == 0:
+    leap_year = False
+elif year % 4 == 0:
+    leap_year = True
+else:
+    leap_year = False
 
-# if leap_year:
-#     print(f'{year} is a leap year')
-# else:
-#     print(f'{year} is not a leap year')
+if leap_year:
+    print(f'{year} is a leap year')
+else:
+    print(f'{year} is not a leap year')
 
-
-example = input('are you a rewards member?') == 'yes'
-
-print(example)
--- The Greatest Showdown.py
+++ The Greatest Showdown.py
@@ -10,13 +10,10 @@
 #     print(f'{num3} is the largest out of the three')
 
 
-#Assignment 2 Task 2
-num1, num2, num3 = int(input('Enter any three numbers:\n')), int(input()), int(input())
-if num1 > num2 and num1 > num3 and num2 > num3:
-    print(f'{num1} is the largest out of the three, and {num3} is the smallest')
-elif num2 > num1 and num2 > num3 and num1 < num3:
-    print(f'{num2} is the largest number, and {num1} is the smallest')
-elif num3 > num2 and num3 > num1 and num2 < num1:
-   print(f'{num3} is the largest out of the three, and {num2} is the smallest')
-else:
-    print(f'{num3} is the largest, and {num1} is the smallest')
+#Assignment 2 Task 23
+num1, num2, num3 = int(input("Enter three numbers:\n")), int(input()), int(input())
+
+largest = max(num1, num2, num3)
+smallest = min(num1,num2,num3)
+if largest:
+    print (f'the largeest number is {largest} and the smallest number is {smallest}')
